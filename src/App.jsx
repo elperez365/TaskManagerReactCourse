e3be0a1f021d1060426fbef6b11d95f8994e9e2d
Romina Trazzi <romina.trazzi@posta.it>
2024-02-08 21:29:17 +0100

    Fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Outlet from "./components/Layouts/Outlet/Outlet";
 import Sidebar from "./components/Layouts/Sidebar/Sidebar";
-import Button from "./components/Reusable/Button/Button";
 
 function App() {
   return (
