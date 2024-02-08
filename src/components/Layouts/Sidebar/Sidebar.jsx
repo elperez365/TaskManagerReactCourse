8958971a56e7716e02e7c4315b077d9d2af5b12b
Giovanni Crudele <crudelegio@gmail.com>
2024-02-08 18:07:41 +0100

    started component

--- src/components/Layouts/Sidebar/Sidebar.jsx
+++ src/components/Layouts/Sidebar/Sidebar.jsx
@@ -1,3 +1,20 @@
+import { DUMMYDATA } from "../../../data/mockData.js";
+import { useState } from "react";
+
 export default function Sidebar() {
-  return <></>;
+  return (
+    <>
+      <h3>Your Projects</h3>
+      <div className="content">
+        <button>+ Add Project</button>
+        <section>
+          {DUMMYDATA.map((project) => (
+            <ul key={project.id}>
+              <li>{project.title}</li>
+            </ul>
+          ))}
+        </section>
+      </div>
+    </>
+  );
 }
