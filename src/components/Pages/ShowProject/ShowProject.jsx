66720c0595a9531991024bb8f18bf3e480e52b4d
Romina Trazzi <romina.trazzi@posta.it>
2024-02-09 16:39:03 +0100

    Fix label

--- src/components/Pages/ShowProject/ShowProject.jsx
+++ src/components/Pages/ShowProject/ShowProject.jsx
@@ -1,32 +1,34 @@
 import Input from "../../Reusable/Input/Input";
-import classes from "./ShowProject..module.css";
+import TaskList from "../../Layouts/TaskList/TaskList";
+import Button from "../../Reusable/Button/Button"
+import classes from "./ShowProject.module.css";
 
 export default function ShowProject({ title, description, dueDate, tasks }) {
   return (
     <div className={classes.show_container}>
+      
       <div className={classes.show_header}>
         <h2>{title}</h2>
-        <button>Clear</button>
+        <Button type="button" action={"erase"}>Clear</Button>
       </div>
+
       <div className={classes.show_body}>
         <p>{dueDate}</p>
         <p>{description}</p>
       </div>
+      
       <hr />
+      
       <div className={classes.input_area}>
-        <Input typeInput="input" label="Task" type="text" />
-        <button>Add Task</button>
+        <div className={classes.input_container}>
+          <Input typeInput="input" label="Task" type="text" />
+          <span>
+            <Button type="button" action={"add"}>Add Task</Button>
+          </span>
+        </div>
+        <TaskList tasks={tasks}/>
       </div>
-      <div className={classes.tasks}>
-        <ul>
-          {tasks.map((task,index) => (
-            <div key={index} className={classes.task}>
-              <li>{task}</li>
-              <button>Clear</button>
-            </div>
-          ))}
-        </ul>
-      </div>
+      
     </div>
   );
 }
