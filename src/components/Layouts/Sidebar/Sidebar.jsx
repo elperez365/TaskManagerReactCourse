0683aa8457e3588c61e1a1ec5a4ff2f55856627b
vasco ceti <vasco.ceti@gmail.com>
2024-02-14 10:11:52 +0100

    responsive ok

--- src/components/Layouts/Sidebar/Sidebar.jsx
+++ src/components/Layouts/Sidebar/Sidebar.jsx
@@ -1,15 +1,32 @@
-
-
 import Button from "../../Reusable/Button/Button";
 import classes from "./Sidebar.module.css";
 import ProjectList from "../ProjectList/ProjectList.jsx";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Sidebar(props) {
   const [store, setStore] = props.store;
+
+  // salva la risoluzione dello schermo in uno state
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [window.innerWidth]);
+
   return (
     <>
       <section className={classes.content}>
-        <h3 className={classes.title}>Your Projects</h3>
+        {width > 600 ? (
+          <h3 className={classes.title}>Your Projects</h3>
+        ) : (
+          <h5 className={classes.title}>Projects</h5>
+        )}
+
         <Button
           onClick={() =>
             setStore((prev) => {
@@ -19,7 +36,7 @@ export default function Sidebar(props) {
           type="button"
           action="create"
         >
-          + Add Project
+          {width > 600 ? " + Add Project" : "+"}
         </Button>
         <div>
           <ProjectList store={[store, setStore]}></ProjectList>
